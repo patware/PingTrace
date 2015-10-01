7a045ce5f9c8bd002ee874126f2c5426668b1b34
patware <patware@hotmail.com>
2015-09-30 22:22:58 -0400

    Removed the Asp.Net V5 project, getting too many reference errors and the paths are just too long.

---
+++ PingTraceSolution/Patware.PingTrace.Core/TraceResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patware.PingTrace.Core
+{
+    public class TraceResult
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Identity { get; set; }
+        public string MachineName { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime FinishedAt { get; set; }
+        public string Payload { get; set; }
+        
+        public TimeSpan Elapsed
+        {
+            get { return FinishedAt - StartedAt; }
+        }
+
+    }
+}
